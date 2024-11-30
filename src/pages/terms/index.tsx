180e55cce506d63e6f69d191f6b3c94a2e8f9a99
noble-6 <torresroger445@gmail.com>
2024-11-30 11:06:16 -0600

    fix errors host db

--- src/pages/terms/index.tsx
+++ src/pages/terms/index.tsx
@@ -1,23 +1,18 @@
-import { useState } from 'react'
-import { useTranslation } from 'next-i18next'
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { CSVLink } from 'react-csv'
+import {useState} from 'react'
+import {useTranslation} from 'next-i18next'
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import Layout from '@/components/layout/admin'
 import Card from '@/components/common/card'
-import Search from '@/components/common/search'
 import LinkButton from '@/components/ui/link-button'
-import { Routes } from '@/config/routes'
-import { useUsersQuery } from '@/data/users'
+import {Routes} from '@/config/routes'
+import {useUsersQuery} from '@/data/users'
 import Loader from '@/components/ui/loader/loader'
 import ErrorMessage from '@/components/ui/error-message'
-import UserList from '@/components/user/user-list'
 import PageHeading from '@/components/common/page-heading'
-import { CsvIcon } from '@/components/icons/csv-icon'
-import Select from '@/components/ui/select/select'
-import { UsersResponse } from '@/types/users'
-import { useRouter } from 'next/router'
+import {UsersResponse} from '@/types/users'
+import {useRouter} from 'next/router'
 import ReactMarkdown from 'react-markdown'
-import { useSettingsQuery } from '@/data/settings'
+import {useSettingsQuery} from '@/data/settings'
 import {
   getAuthCredentials,
   hasAccess,
@@ -25,20 +20,20 @@ import {
   allowedRoles,
   isAuthenticated,
 } from '@/utils/auth-utils'
-import { GetServerSideProps } from 'next'
+import {GetServerSideProps} from 'next'
 
 export default function Users() {
-  const { t } = useTranslation()
+  const {t} = useTranslation()
   const today = new Date().toISOString().split('T')[0]
-  const { settings } = useSettingsQuery()
+  const {settings} = useSettingsQuery()
 
-  const [searchTerm, setSearchTerm] = useState('')
-  const [searchJob, setSearchJob] = useState('')
-  const [page, setPage] = useState(1)
-  const [userFilter, _setUserFilter] = useState<UsersResponse[]>([])
-  const { permissions } = getAuthCredentials()
-  let permission = hasAccess(adminOnly, permissions)
-  const { users, loading, error, paginatorInfo } = useUsersQuery({
+  const [searchTerm,setSearchTerm] = useState('')
+  const [searchJob,setSearchJob] = useState('')
+  const [page,setPage] = useState(1)
+  const [userFilter,_setUserFilter] = useState<UsersResponse[]>([])
+  const {permissions} = getAuthCredentials()
+  let permission = hasAccess(adminOnly,permissions)
+  const {users,loading,error,paginatorInfo} = useUsersQuery({
     limit: 5,
     page,
     search: searchTerm,
@@ -51,7 +46,7 @@ export default function Users() {
 
   if (error) return <ErrorMessage message={error.message} />
 
-  function handleSearch({ searchText }: { searchText: string }) {
+  function handleSearch({searchText}: {searchText: string}) {
     setSearchTerm(searchText)
     setPage(1)
   }
@@ -151,11 +146,11 @@ Si tienes alguna pregunta sobre estos términos y condiciones, por favor contác
 Users.Layout = Layout
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { token, permissions } = getAuthCredentials(ctx)
+  const {token,permissions} = getAuthCredentials(ctx)
   const locale = ctx.locale || 'es'
   if (
-    !isAuthenticated({ token, permissions }) ||
-    !hasAccess(allowedRoles, permissions)
+    !isAuthenticated({token,permissions}) ||
+    !hasAccess(allowedRoles,permissions)
   ) {
     return {
       redirect: {
@@ -167,7 +162,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   return {
     props: {
       userPermissions: permissions,
-      ...(await serverSideTranslations(locale, ['table', 'common', 'form'])),
+      ...(await serverSideTranslations(locale,['table','common','form'])),
     },
   }
 }
